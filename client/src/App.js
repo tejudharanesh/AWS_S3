5769eb977388bef51bd6b1cc7949e63f98970f53
tejudharanesh <tejudharanesh1234@gmail.com>
2024-08-30 17:27:16 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import UploadForm from "./components/UploadForm";
+const App = () => {
+  return (
+    <div>
+      <h1>Upload Images and video to store in cloud</h1>
+      <UploadForm />
+    </div>
+  );
+};
+
+export default App;
